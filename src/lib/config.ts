e5b4bb5d219ed6670c89fc6510cd139e85a4ec73
Ncookie <ncukiert@gmail.com>
2024-07-20 20:56:47 -0300

    ignoring gp booster on optimism

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -13,5 +13,6 @@ export const defaultMetaDescription = `Lure them in with points, and they won't 
 
 export const ignoredAddresses: Lowercase<Address>[] = [
   '0x327b2ea9668a552fe5dec8e3c6e47e540a0a58c6', // GP Booster @ Base
-  '0x1dcfb8b47c2f05ce86c21580c167485de1202e12' // GP Booster @ Arbitrum
+  '0x1dcfb8b47c2f05ce86c21580c167485de1202e12', // GP Booster @ Arbitrum
+  '0xdeef914a2ee2f2014ce401dcb4e13f6540d20ba7' // GP Booster @ Optimism
 ]
